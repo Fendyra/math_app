a66bce4db62248c6cded3f92e806a0a0643cedc6
ZakinandaFaishal <zakinanda.faishal@gmail.com>
2025-09-01 21:52:05 +0700

    baruu

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bermain_logika/main.dart';
+import 'package:math_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
